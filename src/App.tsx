db42fcca6017bae4514d6cff7a6ea37bbcbc4423
dtech-101 <dtechdtech36@gmail.com>
2024-08-14 08:24:45 -0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,47 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import Icon from "./assets/icon.svg";
 import "./App.css";
 import AppRoutes from "./appRoutes";
+import { DisplayCard } from "./components/displayCard";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 2000);
   return (
-    <>
-      <AppRoutes />
-    </>
+    <DisplayCard>
+      {isLoading ? (
+        <div
+          style={{
+            background: "#0A0A0A",
+            height: "100vh",
+            width: "100%",
+          }}
+        >
+          <div
+            style={{
+              position: "relative",
+              width: "100%",
+            }}
+          >
+            <img
+              id="icon"
+              src={Icon}
+              style={{
+                margin: "auto",
+                display: "flex",
+                justifyContent: "center",
+                position: "relative",
+                top: "45vh",
+              }}
+            />
+          </div>
+        </div>
+      ) : (
+        <AppRoutes />
+      )}
+    </DisplayCard>
   );
 }
 
